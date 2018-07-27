7d5ab0ba579ca2027fb54fd7baa5ffc8af175276
Andrew London <alnano2@gmail.com>
2018-07-27 00:35:12 -0700

    ignore not important file

---
+++ mockData.js
@@ -0,0 +1,149 @@
+let data = [{id: "NLEJ7gxOak", title: "Most stockmarket returns come from a tiny fraction of shares", description: "Facebook, Amazon, Apple, Netflix and Google (FAANG) have been the motor of the S&P 500", authors: null, slug: "most-stockmarket-returns-come-from-a-tiny-fraction-of-shares-4212887524", …}
+1
+:
+{id: "1qEyB45zaB", title: "Google, Facebook, Apple, Amazon may pay more to operate in Sunnyvale", description: "Companies that are currently in Sunnyvale, such as…der a plan now circulating in the South Bay city.", authors: Array(1), slug: "google-facebook-apple-amazon-may-pay-more-to-operate-in-sunnyvale-2806969049", …}
+2
+:
+{id: "2ZakjpqNE1", title: "AT&T finished its $81bn takeover of Warner Bros tw… the judge's approval- Technology News, Firstpost", description: "AT&T plans to compete against the likes of Amazon, Google and Netflix.", authors: Array(1), slug: "at-and-t-finished-its-81bn-takeover-of-warner-bros…dges-approval-technology-news-firstpost-123953252", …}
+3
+:
+{id: "3561JA50Go", title: "Amazon employees demand company cut ties with ICE", description: "Their call follows employee-driven campaigns at Mi…ide technology to ICE and to military operations.", authors: Array(1), slug: "amazon-employees-demand-company-cut-ties-with-ice-1297145711", …}
+4
+:
+{id: "NXGwelNvaY", title: "Why FANG stocks are hitting record highs despite US-China trade fights", description: "Facebook, Netflix and the tech-heavy Nasdaq index hit record highs on Wednesday.", authors: Array(1), slug: "why-fang-stocks-are-hitting-record-highs-despite-us-china-trade-fights-1610295314", …}
+5
+:
+{id: "dNx6QyO62L", title: "Netflix, Microsoft, and Google just quietly changed how the web works", description: "The organization that sets standards for the web j… failed to beat back a stupid, greedy technology.", authors: Array(1), slug: "netflix-microsoft-and-google-just-quietly-changed-how-the-web-works-4086441722", …}
+6
+:
+{id: "5KGXvjpkaA", title: "Twitter acquires anti-abuse technology provider Smyte", description: "Twitter this morning announced it has agreed to bu…ts. Terms of the deal were not disclosed, but thi", authors: Array(1), slug: "twitter-acquires-anti-abuse-technology-provider-smyte-3154506676", …}
+7
+:
+{id: "yN6dopza5K", title: "Tech’s Titans Tiptoe Toward Monopoly", description: "Will Facebook, Google and Amazon be regulated or b…epends on how they grow, writes Christopher Mims.", authors: Array(1), slug: "techs-titans-tiptoe-toward-monopoly-87886255", …}
+8
+:
+{id: "QmDGzKYENl", title: "I invested early in Google and Facebook. Now they terrify me.", description: "The growing Internet monopolies have grabbed so mu…they are a menace to public health and democracy.", authors: Array(2), slug: "i-invested-early-in-google-and-facebook-now-they-terrify-me-2731203877", …}
+9
+:
+{id: "w2EWxOZVae", title: "Amazon Workers Demand Jeff Bezos Cancel Face Recognition Contracts With Law Enforcement", description: "Following employee protests at Google and Microsof…ata-mining firm Palantir from its cloud services.", authors: Array(1), slug: "amazon-workers-demand-jeff-bezos-cancel-face-recognition-contracts-with-law-enforcement-3587880057", …}
+10
+:
+{id: "Bx6mkYR5Eg", title: "U.S. senator questions Alphabet, Twitter on Huawei relationship", description: "A U.S. senator on Thursday is seeking responses fr…llowing a disclosure from Facebook Inc this week.", authors: Array(1), slug: "u-s-senator-questions-alphabet-twitter-on-huawei-relationship-2854277886", …}
+11
+:
+{id: "wxaOxRKK6l", title: "Google, Microsoft, Amazon: Are they responsible for government use?", description: "Welcome to the world of politicized capitalism doused with hashtag activism.", authors: Array(1), slug: "google-microsoft-amazon-are-they-responsible-for-government-use-2411585020", …}
+12
+:
+{id: "LqEZDKbr6g", title: "Is insurance a rich enough game to disrupt?", description: "For the last decade, the largest technology compan…eyeing innovation across the insurance landscape.", authors: Array(2), slug: "Is-insurance-a-rich-enough-game-to-disrupt-Uj4qPiI", …}
+13
+:
+{id: "rgGPBO9aLN", title: "Tech’s Two Philosophies", description: "Google and Facebook represent one philosophy, and …ly platforms are more important than aggregators.", authors: Array(1), slug: "techs-two-philosophies-2525415731", …}
+14
+:
+{id: "48pG2BdGxy", title: "Iran's blogfather: Facebook, Instagram and Twitter are killing the web", description: "Hossein Derakhshan was imprisoned by the regime fo…ad serving up a stream of pointless social trivia", authors: Array(1), slug: "irans-blogfather-facebook-instagram-and-twitter-are-killing-the-web-1673171813", …}
+15
+:
+{id: "2P6jxX3Epx", title: "Apple escalates war against Facebook, but doesn't mention it at WWDC", description: "ITP2 anti-tracking and limiting Screen Time app clearly targeting Facebook and Instagram", authors: Array(1), slug: "apple-escalates-war-against-facebook-but-doesnt-mention-it-at-wwdc-415504263", …}
+16
+:
+{id: "k1qEy9dEB9", title: "Snapchat Launches a Colossal Expansion of Its Advertising, Ushering in a New Era for the App", description: "Imran Khan was an internet-focused investment bank…inese ecommerce giant Alibaba's high-profile IPO.", authors: Array(1), slug: "snapchat-launches-a-colossal-expansion-of-its-adve…sing-ushering-in-a-new-era-for-the-app-1624878898", …}
+17
+:
+{id: "RdG0OpAA6g", title: "Apple Hands Siri Responsibilities to Executive Poached From Google", description: "Apple is handing oversight of its Siri team to new… three months after hiring him from rival Google.", authors: Array(2), slug: "Apple-Hands-Siri-Responsibilities-to-Executive-Poached-From-Google-u5leUAV", …}
+18
+:
+{id: "4m6b2q9q6l", title: "Netflix won’t stream real games, but EA, Google, and Microsoft will", description: "The tech world was buzzing today with a report tha… like the video subscription giant was planning …", authors: Array(1), slug: "netflix-wont-stream-real-games-but-ea-google-and-microsoft-will-1431246832", …}
+19
+:
+{id: "qmElAJNeaA", title: "Facebook and Twitter: we can do more to protect disabled people", description: "MPs question social media giants following criticism from Katie Price over abuse of son", authors: Array(1), slug: "facebook-and-twitter-we-can-do-more-to-protect-disabled-people-2054218776", …}
+20
+:
+{id: "24av88KzGB", title: "After Scrutinizing Facebook, Congress Turns to Google Deal With Huawei", description: "Members of Congress have begun scrutinizing Google…ngton’s escalating digital cold war with Beijing.", authors: Array(1), slug: "after-scrutinizing-facebook-congress-turns-to-google-deal-with-huawei-3038284201", …}
+21
+:
+{id: "BKG3gBeGmR", title: "Australia drafts laws forcing Facebook and Google to reveal encrypted data", description: "Security agencies would be given access to encrypted messaging apps under bid to ‘modernise’ laws", authors: Array(1), slug: "australia-drafts-laws-forcing-facebook-and-google-to-reveal-encrypted-data-974181326", …}
+22
+:
+{id: "3561m34Y6o", title: "How Tech Companies Are Trying to Disrupt Terrorist Social Media Activity", description: "Google, Facebook, Twitter and Microsoft formed the…prevent terrorists from exploiting their services", authors: Array(7), slug: "How-Tech-Companies-Are-Trying-to-Disrupt-Terrorist-Social-Media-Activity", …}
+23
+:
+{id: "7e6gd2DQGn", title: "It’s time to rein in the data barons", description: "Facebook, Amazon, and Google will resist attempts …al privacy, it’s a fight we can’t afford to lose.", authors: Array(1), slug: "its-time-to-rein-in-the-data-barons-1636539000", …}
+24
+:
+{id: "mDGzk7OENl", title: "The Apple-Facebook war is heating up. We may not like who wins.", description: "A new rivalry between tech giants focuses on user privacy.", authors: Array(1), slug: "the-apple-facebook-war-is-heating-up-we-may-not-like-who-wins-658752228", …}
+25
+:
+{id: "mDGzjneWGN", title: "'Google And Facebook Won': Old Guard Of Advertising Under Threat In Cannes", description: "Internet giants’ growing control of ad spend brought a sense of change to Cannes Lions festival", authors: Array(1), slug: "google-and-facebook-won-old-guard-of-advertising-under-threat-in-cannes-810058702", …}
+26
+:
+{id: "796Kwz2Q6M", title: "Frustrated with Facebook, Google, Tesla? So are their shareholders", description: "In the past week, the three companies’ investors s…ountability from the companies and their leaders.", authors: Array(1), slug: "frustrated-with-facebook-google-tesla-so-are-their-shareholders-695300461", …}
+27
+:
+{id: "Wy65DZb4aw", title: "Global Internet Advertising Market 2018: Top playe…et, Facebook, Baidu, Yahoo! Inc, Alibaba, Twitter", description: "Global Internet Advertising Market Research Report…iding complete analysis of the industry for the …", authors: Array(1), slug: "global-internet-advertising-market-2018-top-player…acebook-baidu-yahoo-inc-alibaba-twitter-432939319", …}
+28
+:
+{id: "8z6DVOlzEm", title: "Google flips switch on Chrome's newest defensive technology", description: "With 'Site Isolation' in use, the browser should b…esigned to steal info such as log-on credentials.", authors: Array(1), slug: "Google-flips-switch-on-Chromes-newest-defensive-technology-XrU9oGq", …}
+29
+:
+{id: "7e6gV4zPan", title: "Apple strikes blow to Facebook as it clamps down on data harvesting", description: "Rules appear to target services like Onavo Protect…ser data even as it feeds information to Facebook", authors: Array(1), slug: "apple-strikes-blow-to-facebook-as-it-clamps-down-on-data-harvesting-908622714", …}
+30
+:
+{id: "z28aeM5anR", title: "Technology Unlocks “Economies Of Unscale” For Small Businesses", description: "Twenty years ago, small business owners lacked the…der of Stripe, once told me, the company’s large…", authors: Array(1), slug: "technology-unlocks-economies-of-unscale-for-small-businesses-2055432894", …}
+31
+:
+{id: "WnaplnwaXq", title: "Marqeta raised $45M led by Iconiq for payment card…ssing technology, reportedly at a $545M valuation", description: "On the heels of payment processing startup Adyen p…uding instant card-based payments and issuing the", authors: Array(1), slug: "marqeta-raised-45m-led-by-iconiq-for-payment-card-…hnology-reportedly-at-a-545m-valuation-3700172422", …}
+32
+:
+{id: "8z6DLwNm6m", title: "Amazon employees protest sale of facial recognition software to police", description: "The workers also want Amazon to stop selling AWS services that support deportations by ICE", authors: Array(1), slug: "amazon-employees-protest-sale-of-facial-recognition-software-to-police-3116957208", …}
+33
+:
+{id: "24avg35Z6B", title: "The Legendary Apple Research Group That Shaped Our World", description: "Before Steve Jobs gave it the ax in 1997, the Adva…f the most influential technology of the century.", authors: Array(1), slug: "the-legendary-apple-research-group-that-shaped-our-world-776782172", …}
+34
+:
+{id: "3561ZY1dGo", title: "Aaptiv raises $22M from Amazon, Disney and more for its “Netflix for fitness”, now valued over $200M", description: "Health and wellness has been one of the biggest ca…s users a selection of audio-based, personal-tra…", authors: Array(1), slug: "aaptiv-raises-22m-from-amazon-disney-and-more-for-…tflix-for-fitness-now-valued-over-200m-3618045289", …}
+35
+:
+{id: "5KGX3nlY6A", title: "ACLU wants Amazon to stop offering surveillance technology", description: "Their message is loud and clear.", authors: Array(1), slug: "aclu-wants-amazon-to-stop-offering-surveillance-technology-2427880665", …}
+36
+:
+{id: "5KGXy45kEA", title: "ACLU wants Amazon to stop offering surveillance technology", description: "Their message is loud and clear.", authors: Array(1), slug: "aclu-wants-amazon-to-stop-offering-surveillance-technology-25877570", …}
+37
+:
+{id: "mDGz2jLGNl", title: "Adyen aims for a $1B IPO, valuing the payments startup at up to $8.3B when it lists on June 13", description: "After announcing its intention to go public in May…raising between €922 million and €947 million […]", authors: Array(1), slug: "adyen-aims-for-a-1b-ipo-valuing-the-payments-start…at-up-to-8-3b-when-it-lists-on-june-13-2051945583", …}
+38
+:
+{id: "Nx6QxAeWa2", title: "Amazon Workers to Jeff Bezos: Stop Weaponizing Our Tech", description: "A growing tide of Silicon Valley workers are rebel…r employers over practices they see as unethical.", authors: Array(1), slug: "amazon-workers-to-jeff-bezos-stop-weaponizing-our-tech-550668188", …}
+39
+:
+{id: "A56VO04GP2", title: "Norwest just scored an interesting new partner: Google and Facebook alum Priti Youssef Choksi", description: "A lot of people who’ve been working in the venture…artner of the multi-stage investment firm Norwes…", authors: Array(1), slug: "norwest-just-scored-an-interesting-new-partner-goo…-and-facebook-alum-priti-youssef-choksi-550433663", …}
+40
+:
+{id: "2P6jr2q36p", title: "Amazon Employees Want Jeff Bezos to Stop Selling Rekognition to Law Enforcement, Says Report", description: "They join shareholders and the ACLU in their concern.", authors: Array(1), slug: "amazon-employees-want-jeff-bezos-to-stop-selling-r…gnition-to-law-enforcement-says-report-1125187638", …}
+41
+:
+{id: "jLaRvY3qag", title: "Apple Isn't Your Friend", description: "Apple has been steadily positioning itself as the …ave us, and now’s the time to keep your guard up.", authors: Array(1), slug: "apple-isnt-your-friend-1678119617", …}
+42
+:
+{id: "mDGzvwQeGN", title: "Amazon to add over 1,000 jobs in Ireland in country's biggest staff...", description: "Amazon will add more than 1,000 new jobs in Irelan…tinues to be a magnet for major technology firms.", authors: Array(1), slug: "amazon-to-add-over-1-000-jobs-in-ireland-in-countrys-biggest-staff-523838389", …}
+43
+:
+{id: "e8z6Dz3Emq", title: "Why Eric Schmidt doesn’t know how Google works", description: "If Google’s management style is the reason for its…or been financially inefficient and unprofitable?", authors: Array(2), slug: "why-eric-schmidt-doesnt-know-how-google-works-618045474", …}
+44
+:
+{id: "5KGXbe1jaA", title: "Mapbox taps ex-Apple designer to head augmented reality efforts", description: "Mapping startup Mapbox Inc said Monday that it had…design location-based augmented reality software.", authors: Array(1), slug: "mapbox-taps-ex-apple-designer-to-head-augmented-reality-efforts-512406193", …}
+45
+:
+{id: "mNLEJK1akQ", title: "Facebook and Twitter are being used to manipulate public opinion – report", description: "Nine-country study finds widespread use of social …ion and propaganda by governments and individuals", authors: Array(1), slug: "facebook-and-twitter-are-being-used-to-manipulate-public-opinion-report-1276645301", …}
+46
+:
+{id: "dNx6QngG2L", title: "Tech Stocks Are Hitting Highs as Economic Uncertainty Rises", description: "Investors have returned to the safety and growth of the biggest technology stocks.", authors: Array(1), slug: "tech-stocks-are-hitting-highs-as-economic-uncertainty-rises-3108831056", …}
+47
+:
+{id: "W9a41ly760", title: "Amazon is killing the TV remote", description: "Amazon's new Fire TV Cube could become the new standard for watching TV.", authors: Array(1), slug: "amazon-is-killing-the-tv-remote-2016255775", …}
+48
+:
+{id: "Nx6Q89ga2L", title: "Tech stocks mixed after recent surge; banks lift S&P, Dow", description: "Gains for Amazon and Tesla helped the Nasdaq open … Facebook led a decline in the technology sector.", authors: Array(1), slug: "tech-stocks-mixed-after-recent-surge-banks-lift-s-and-p-dow-3379641361", …}
+49
+:
+{id: "NXGw9XKDaY", title: "Lawmakers press Apple and Google to explain how they track and listen to users", description: "Top members of the House Energy and Commerce Commi… their smartphone operating systems collect data.", authors: Array(2), slug: "Lawmakers-press-Apple-and-Google-to-explain-how-they-track-and-listen-to-users-ItAfH40", …}
+]
